28b105989c6fffd9aaa27a52cc79778775d0ff5a
Jesu Castillo <castillojesuj@gmail.com>
2019-08-06 00:36:57 -0400

    add resolvers

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,10 +9,40 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+function products() {
+  return Product.find({}).exec()
+}
+
+function product(_, { id }, ctx, info) {
+  return Product.findById(id).exec()
+}
+
+function newProduct(_, { input: product }, { user }, info) {
+  return new Product({ ...product, createdBy: user }).save()
+}
+
+function updateProduct(_, { id, input: update }, ctx, info) {
+  return Product.findOneAndUpdate({ _id: id }, update, { new: true }).exec()
+}
+
+function removeProduct(_, { id }, ctx, info) {
+  return Product.findOneAndDelete({ _id: id }).exec()
+}
+
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    products,
+    product
+  },
+  Mutation: {
+    newProduct,
+    updateProduct,
+    removeProduct
+  },
   Product: {
-    __resolveType(product) {}
+    __resolveType(product) {},
+    createdBy({ createdBy: id }, args, ctx, info) {
+      return User.findById(id).exec()
+    }
   }
 }
